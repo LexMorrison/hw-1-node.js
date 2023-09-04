bedaf34d843bbf3eab411fac73268c6d74cbe502
LexMorrison <borrlex11@gmail.com>
2023-09-04 10:23:02 +0200

    deleted some comments

--- contacts.js
+++ contacts.js
@@ -1,27 +1,25 @@
-// contacts.js
 import fs from "fs/promises";
 import { nanoid } from "nanoid";
 import path from "path";
-/*
- * Раскомментируй и запиши значение */
+
 const contactsPath = path.resolve("db", "contacts.json");
 
 // TODO: задокументировать каждую функцию
 export const listContacts = async () => {
-  // ...твой код. Возвращает массив контактов.
+  //  Возвращает массив контактов.
   const data = await fs.readFile(contactsPath, "utf-8");
   return JSON.parse(data);
 };
 
 export const getContactById = async (id) => {
-  // ...твой код. Возвращает объект контакта с таким id. Возвращает null, если объект с таким id не найден.
+  //  Возвращает объект контакта с таким id. Возвращает null, если объект с таким id не найден.
   const contacts = await listContacts();
   const result = contacts.find((item) => item.id === id);
   return result || null;
 };
 
 export const removeContact = async (id) => {
-  // ...твой код. Возвращает объект удаленного контакта. Возвращает null, если объект с таким id не найден.
+  // Возвращает объект удаленного контакта. Возвращает null, если объект с таким id не найден.
   const contacts = await listContacts();
   const index = contacts.findIndex((item) => item.id === id);
   if (index === -1) {
@@ -34,7 +32,7 @@ export const removeContact = async (id) => {
 };
 
 export const addContact = async ({ name, email, phone }) => {
-  // ...твой код. Возвращает объект добавленного контакта.
+  //  Возвращает объект добавленного контакта.
   const contactsList = await listContacts();
   const newContact = {
     id: nanoid(),
